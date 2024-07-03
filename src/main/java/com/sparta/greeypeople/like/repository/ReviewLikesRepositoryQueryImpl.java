3fbbefc9a837f17158f62558a1cf3d850bc8534d
박시현 <sihyun615@naver.com>
2024-07-03 23:43:02 +0900

    🎉 add : 사용자가 등록한 리뷰 좋아요 개수 조회 쿼리 생성

---
+++ src/main/java/com/sparta/greeypeople/like/repository/ReviewLikesRepositoryQueryImpl.java
@@ -0,0 +1,28 @@
+package com.sparta.greeypeople.like.repository;
+
+import static com.sparta.greeypeople.like.entity.QReviewLikes.reviewLikes;
+
+import com.querydsl.core.types.dsl.Wildcard;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class ReviewLikesRepositoryQueryImpl implements ReviewLikesRepositoryQuery {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public long countReviewLikesByUserId(Long userId) {
+        Long count = jpaQueryFactory
+            .select(Wildcard.count)
+            .from(reviewLikes)
+            .where(reviewLikes.user.id.eq(userId))
+            .fetchOne();  //레코드의 개수를 세는 쿼리의 경우 단일 숫자 값을 반환하므로 fetchOne 사용
+
+        // fetchOne()은 쿼리 결과가 없을 경우 null을 반환하므로 Optional을 사용하여 안전하게 처리
+        return Optional.ofNullable(count).orElse(0L);
+    }
+}
