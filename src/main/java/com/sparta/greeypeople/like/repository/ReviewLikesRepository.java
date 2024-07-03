f5371a1bc6f7a1e12cc099aa7b4e15cf9d04e2d6
박시현 <sihyun615@naver.com>
2024-07-03 23:43:35 +0900

    ✨ update : ReviewLikesRepositoryQuery를 extends하도록 추가

--- src/main/java/com/sparta/greeypeople/like/repository/ReviewLikesRepository.java
+++ src/main/java/com/sparta/greeypeople/like/repository/ReviewLikesRepository.java
@@ -4,11 +4,10 @@ import com.sparta.greeypeople.like.entity.ReviewLikes;
 import com.sparta.greeypeople.review.entity.Review;
 import com.sparta.greeypeople.user.entity.User;
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.RepositoryDefinition;
 
-@Repository
-public interface ReviewLikesRepository extends JpaRepository<ReviewLikes, Long> {
+@RepositoryDefinition(domainClass = ReviewLikes.class, idClass = Long.class)
+public interface ReviewLikesRepository extends JpaRepository<ReviewLikes, Long>, ReviewLikesRepositoryQuery {
     Optional<ReviewLikes> findByUserAndReview(User foundUser, Review foundReview);
 }
